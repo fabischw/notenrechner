bbbe908cdef453d96da3bdf8dce59472feff47bb
Fabian <fabischw135@gmail.com>
2022-09-01 08:33:00 +0200

    Removed planing folder from .gitignore, tracking entire project now, added some blank backend files for better planing structure

---
+++ backend/database/database_functions.py
@@ -0,0 +1,14 @@
+#Zentrale Sammlung von FUnktionen für die zugehörige OracleDB (Oracle 21c [XE])
+
+#Hier werden alle Funktionen des Back-und Frontends final mit der Datenbank verbunden über die Schnittstelle oracledb User NOTENRECHNER
+
+
+
+
+
+
+import oracledb
+
+
+
+
