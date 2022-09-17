589d710ad72e04b74e9251fd1a4090f288d29c53
fabischw <107812436+fabischw@users.noreply.github.com>
2022-09-17 11:21:19 +0200

    Update cre_notenrechner_db_structure.sql
    Updated the script for creating the notenrechner database structure to include the system tables

--- backend/database/cre_notenrechner_db_structure.sql
+++ backend/database/cre_notenrechner_db_structure.sql
@@ -1141,4 +1141,232 @@ create index IDX_kursstundenref02 on kursstundenref (kursstundenref_id,kurs_id,s
     maxextents unlimited
   );
   
+
+  
+--LOG table
+
+
+-- Create sequence 
+create sequence S_LOG
+minvalue 1
+maxvalue 9999999999
+start with 1
+increment by 1
+cache 10
+cycle;
+
+
+-- Create table
+create table LOGS
+(
+  LOG_id                        NUMBER(10) not null,
+  table_id                      NUMBER(10) not null,
+  updt_user                     VARCHAR2(30) ,
+  update_type                   VARCHAR2(30) ,
+  message                       VARCHAR2(50) ,
+  cre_userid                    VARCHAR2(30) not null,
+  cre_date                      DATE not null,
+  chg_userid                    VARCHAR2(30),
+  chg_date                      DATE
+)
+
+tablespace users
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+-- Add comments to the columns 
+comment on column LOGS.LOG_id is 'ID LOG';
+comment on column LOGS.table_id is 'table ID';
+comment on column LOGS.updt_user is 'Username des update Users';
+comment on column LOGS.update_type is 'Update typ';
+comment on column LOGS.message is 'additional message';
+comment on column LOGS.cre_date is 'Zeitpunkt der Eintragung';
+comment on column LOGS.cre_userid is 'USER_ID der Eintragung';
+comment on column LOGS.chg_date is 'mögliches Änderungsdatum der eingetragenen Daten';
+comment on column LOGS.chg_userid is 'möglicher Änderungsuser der eingetragenen Daten';
+
+
+create unique index IDX_PK_LOG01 on LOG (LOG_id)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+--Index für Daten, die oft abgerufen werden
+create index IDX_LOG02 on LOG (LOG_id,table_id,updt_user,update_type,cre_date,cre_userid,chg_date,chg_userid)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+  
+ --DEL table 
+ 
+ 
+ -- Create sequence 
+create sequence S_DELL
+minvalue 1
+maxvalue 9999999999
+start with 1
+increment by 1
+cache 10
+cycle;
+
+
+-- Create table
+create table DELLL
+(
+  DELL_id                       NUMBER(10) not null,
+  memory_location               VARCHAR2(500) ,
+  cre_userid                    VARCHAR2(30) not null,
+  cre_date                      DATE not null,
+  chg_userid                    VARCHAR2(30),
+  chg_date                      DATE
+)
+
+tablespace users
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+-- Add comments to the columns 
+comment on column DELL.DELL_id is 'ID DELL';
+comment on column DELL.memory_location is 'Speicherort der gelöschten Daten';
+comment on column DELL.cre_date is 'Zeitpunkt der Eintragung';
+comment on column DELL.cre_userid is 'USER_ID der Eintragung';
+comment on column DELL.chg_date is 'mögliches Änderungsdatum der eingetragenen Daten';
+comment on column DELL.chg_userid is 'möglicher Änderungsuser der eingetragenen Daten';
+
+
+create unique index IDX_PK_DELL01 on DELL (DELL_id)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+--Index für Daten, die oft abgerufen werden
+create index IDX_DELL02 on DELL (DELL_id,memory_location,cre_date,cre_userid,chg_date,chg_userid)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+ 
+ 
+  
+  --TABLES table
+  
+  
+  -- Create sequence 
+create sequence S_TABLESS
+minvalue 1
+maxvalue 9999999999
+start with 1
+increment by 1
+cache 10
+cycle;
+
+
+-- Create table
+create table TABLESS
+(
+  TABLESS_id                    NUMBER(10) not null,
+  table_name                    VARCHAR(50)not null ,
+  cre_userid                    VARCHAR2(30) not null,
+  cre_date                      DATE not null,
+  chg_userid                    VARCHAR2(30),
+  chg_date                      DATE
+)
+
+tablespace users
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+  
+-- Add comments to the columns 
+comment on column TABLESS.TABLESS_id is 'ID TABLESS';
+comment on column TABLESS.table_name is 'Name des tables';
+comment on column TABLESS.cre_date is 'Zeitpunkt der Eintragung';
+comment on column TABLESS.cre_userid is 'USER_ID der Eintragung';
+comment on column TABLESS.chg_date is 'mögliches Änderungsdatum der eingetragenen Daten';
+comment on column TABLESS.chg_userid is 'möglicher Änderungsuser der eingetragenen Daten';
+
+
+create unique index IDX_PK_TABLESS01 on TABLESS (TABLESS_id)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+--Index für Daten, die oft abgerufen werden
+create index IDX_TABLESS02 on TABLESS (TABLESS_id,table_name,cre_date,cre_userid,chg_date,chg_userid)
+  tablespace users
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    next 64K
+    minextents 1
+    maxextents unlimited
+  );
+   
   
