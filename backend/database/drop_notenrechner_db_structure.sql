ef1e570b036693dc398241f24c22c501ec09ff8b
fabischw <107812436+fabischw@users.noreply.github.com>
2022-09-18 16:24:35 +0200

    Added drop_notenrechner_db_structures

---
+++ backend/database/drop_notenrechner_db_structure.sql
@@ -0,0 +1,98 @@
+--table noten
+drop table noten
+drop sequence S_noten
+
+
+-- table lehrer
+drop table lehrer
+drop sequence S_lehrer
+
+
+--table schueler
+drop table schueler
+drop sequence S_schueler
+
+
+--table fach
+drop table fach
+drop sequence S_fach
+
+
+--table stunden
+drop table stunden
+drop sequence S_stunden
+
+
+--table arbeiten
+drop table arbeiten
+drop sequence S_arbeiten
+
+
+--table schulevents
+drop table schulevents
+drop sequence S_schulevents
+
+
+--table kalender
+drop table kalender
+drop sequence S_kalender
+
+
+--table kurs
+drop table kurs
+drop sequence S_kurs
+
+
+--table notenschuelerref
+drop table notenschuelerref
+drop sequence S_notenschuelerref
+
+
+--table lehrerfachref
+drop table lehrerfachref
+drop sequence S_lehrerfachref
+
+
+--table kursschuelerref
+drop table kursschuelerref
+drop sequence S_kursschuelerref
+
+
+--table kursschuleventsref
+drop table kursschuleventsref
+drop sequence S_kursschuleventsref
+
+
+--table kursstundenref
+drop table kursstundenref
+drop sequence S_kursstundenref
+
+
+--table LOGS
+drop table LOGS
+drop sequence S_LOGS
+
+
+--table DELL
+drop table DELL
+drop sequence S_DELL
+
+
+--table TABLESS
+drop table TABLESS
+drop sequence S_TABLESS
+
+
+--table USERR
+drop table USERR
+drop sequence S_USERR
+
+
+--table PERMISSIONN
+drop table PERMISSIONN
+drop sequence S_PERMISSIONN
+
+
+--table USERRUSERRPERMISSIONNREFNREF
+drop table USERRUSERRPERMISSIONNREFNREF
+drop sequence S_USERRUSERRPERMISSIONNREFNREF
